971ed99196540e41a034c79370600035b5e202cd
Akash Sabale <akashsabale100@gmail.com>
2024-07-25 12:37:12 +0530

    first commit

---
+++ components/Categories.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Image, ScrollView, Text } from "react-native";
+import CategoryCard from "./CategoryCard";
+
+const Categories = () => {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 5,
+      }}
+    >
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="category 3"
+      />
+    </ScrollView>
+  );
+};
+
+export default Categories;
