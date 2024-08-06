38bec2868d0396601fe44754d2875ea5b40e0b6c
Akash Sabale <akashsabale100@gmail.com>
2024-08-06 16:58:09 +0530

    Implemen;ted Redux toolkit for state management of items in basket

--- components/DishRow.jsx
+++ components/DishRow.jsx
@@ -1,15 +1,31 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import Currency from "react-currency-formatter";
+// import Currency from "react-currency-formatter";
 import { urlFor } from "../sanity";
-import {
-  CurrencyRupeeIcon,
-  MinusCircleIcon,
-  PlusCircleIcon,
-} from "react-native-heroicons/outline";
+import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
+import { CurrencyRupeeIcon } from "react-native-heroicons/outline";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItemsWithId,
+} from "../features/basketSlice";
 
 const DishRow = ({ id, name, description, price, image }) => {
+  const items = useSelector((state) => selectBasketItemsWithId(state, id));
+  const dispatch = useDispatch();
+
   const [isPressed, setIsPressed] = useState(false);
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+
+    dispatch(removeFromBasket({ id }));
+  };
 
   return (
     <>
@@ -23,16 +39,19 @@ const DishRow = ({ id, name, description, price, image }) => {
           <View className="flex-1 pr-2">
             <Text className="text-lg mb-1">{name}</Text>
             <Text className="text-gray-400">{description}</Text>
-            <Text className="text-gray-400 mt-2">
-              <Currency quantity={price} currency="INR" />
-              {/* <CurrencyRupeeIcon size={22} color="green" /> */}
-            </Text>
+            <View className="flex flex-row items-center mt-2 space-x-1">
+              <CurrencyRupeeIcon size={20} color="lightgray" />
+              <Text className="text-gray-400">
+                {/* <Currency quantity={price} currency="INR" /> */}
+                {price}
+              </Text>
+            </View>
           </View>
 
           <View>
             <Image
               source={{ uri: urlFor(image).url() }}
-              className="h-20 w-20 bg-gray-300 p-4"
+              className="h-20 w-20 bg-gray-300 p-4 rounded-md"
               style={{
                 borderWidth: 1,
                 borderColor: "#F3F3F4",
@@ -45,13 +64,19 @@ const DishRow = ({ id, name, description, price, image }) => {
       {isPressed && (
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
-            <TouchableOpacity>
-              <MinusCircleIcon color="#00CCBB" size={40} />
+            <TouchableOpacity
+              onPress={removeItemFromBasket}
+              disabled={!items.length}
+            >
+              <MinusCircleIcon
+                color={items.length > 0 ? "#00CCBB" : "gray"}
+                size={40}
+              />
             </TouchableOpacity>
 
-            <Text>0</Text>
+            <Text>{items.length}</Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={addItemToBasket}>
               <PlusCircleIcon color="#00CCBB" size={40} />
             </TouchableOpacity>
           </View>
